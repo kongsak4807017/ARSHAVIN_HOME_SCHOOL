5c7bf1701d5f1def32fd4e6e3631d537f8785623
kongsak4807017 <78156120+kongsak4807017@users.noreply.github.com>
2026-07-19 15:16:59 +0700

    feat: cache first lesson for offline use

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE = 'arshavin-grade4-v1';
+const CORE = [
+  './',
+  './index.html',
+  './assets/css/site.css',
+  './assets/js/sleep-lesson.js',
+  './subjects/human-body/sleep-ready-brain.html',
+  './worksheets/student/sleep-ready-brain-a4.html',
+  './manifest.webmanifest'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match('./index.html'))));
+});
